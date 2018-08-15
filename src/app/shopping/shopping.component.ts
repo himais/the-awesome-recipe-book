eb24e5157a5add743f48d68003e60110a7f08a95
Maíra Tabata <ink.dheart@gmail.com>
2018-08-15 12:05:46 -0300

    feat (S2A53): Adiciona estrutura para a compra de ingredientes

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Sugar', 3)
+  ];
 
   constructor() { }
 
