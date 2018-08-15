6d7b5e14d162f3435d865f00e0a82ddf5cb97d4c
Maíra Tabata <ink.dheart@gmail.com>
2018-08-15 11:51:36 -0300

    refactor: Nome do arquivo renomeado de module => model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.module';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
