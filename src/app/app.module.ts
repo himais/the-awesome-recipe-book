b80f1bee5949be8cc04be74e21c793dcb0a88703
Maíra Tabata <ink.dheart@gmail.com>
2018-08-15 10:15:47 -0300

    feat (S2A43): Adiciona componente para o cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
