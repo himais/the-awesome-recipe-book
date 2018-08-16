705d34f95050fdcd90564c71b056b5303d9768e5
Maíra Tabata <ink.dheart@gmail.com>
2018-08-16 12:27:34 -0300

    feat (S6A80): Exibe detalhes da receita escolhida na lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingComponent } from './shopping/shopping.component';
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  @Output('onRecipeSelected') selectedRecipetoShow = new EventEmitter<Recipe>();
 
   constructor() { 
     this.recipes.push(new Recipe("The new recipe 0", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
@@ -15,6 +16,10 @@ export class RecipeListComponent implements OnInit {
     this.recipes.push(new Recipe("The new recipe 2", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
     this.recipes.push(new Recipe("The new recipe 3", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
     this.recipes.push(new Recipe("The new recipe 4", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+  }
+  
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipetoShow.emit(recipe);
   }
 
   ngOnInit() {
