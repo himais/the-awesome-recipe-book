92dcce038403bbaf7fad4c364d219f43ddaa44d8
Maíra Tabata <ink.dheart@gmail.com>
2018-08-16 01:58:28 -0300

    feat (S6A79): Adiciona comunicação entre recipelist e recipeitem

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,11 +10,11 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
 
   constructor() { 
-    this.recipes.push(new Recipe("The new recipe", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
-    this.recipes.push(new Recipe("The new recipe", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
-    this.recipes.push(new Recipe("The new recipe", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
-    this.recipes.push(new Recipe("The new recipe", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
-    this.recipes.push(new Recipe("The new recipe", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+    this.recipes.push(new Recipe("The new recipe 0", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+    this.recipes.push(new Recipe("The new recipe 1", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+    this.recipes.push(new Recipe("The new recipe 2", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+    this.recipes.push(new Recipe("The new recipe 3", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
+    this.recipes.push(new Recipe("The new recipe 4", "the desc", "https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=750&q=80"));
   }
 
   ngOnInit() {
